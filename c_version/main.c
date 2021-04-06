202c87afe3e467fe766163aedaff5022ba791dce
mentalblood <neceporenkostepan@gmail.com>
2021-04-06 14:26:45 +0300

    (param)(ref) now working properly

--- c_version/main.c
+++ c_version/main.c
@@ -172,14 +172,15 @@ int compile__def_lengths[2] = {11, 16};
 	compile__def_memcpy(1);\
 }
 
-char *compile__if_strings[3] = {"if '", "' in ", ":\n"};
-int compile__if_lengths[3] = {4, 5, 2};
+char *compile__if_strings[4] = {"if ", " and '", "' in ", ":\n"};
+int compile__if_lengths[4] = {3, 6, 5, 2};
 #define compile__if_memcpy(i) compile__memcpy(compile__if_strings[i], compile__if_strings[i] + compile__if_lengths[i])
 
 #define compile__cpy_if(param_name_start, param_name_end) {\
-	compile__if_memcpy(0); compile__memcpy(param_name_start, param_name_end);\
-	compile__if_memcpy(1); compile__memcpy(template_name, template_name_end);\
-	compile__if_memcpy(2);\
+	compile__if_memcpy(0); compile__memcpy(template_name, template_name_end);\
+	compile__if_memcpy(1); compile__memcpy(param_name_start, param_name_end);\
+	compile__if_memcpy(2); compile__memcpy(template_name, template_name_end);\
+	compile__if_memcpy(3);\
 }
 
 char *compile__return = "\treturn result\n";

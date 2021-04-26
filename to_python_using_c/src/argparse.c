ad0ce84202edc742a4813cab6523e33cf53f4fe0
mentalblood <neceporenkostepan@gmail.com>
2021-04-26 14:06:27 +0300

    changed initial list allocation size to 2

--- to_python_using_c/src/argparse.c
+++ to_python_using_c/src/argparse.c
@@ -1,3 +1,5 @@
+// -bool_arg_name -str_arg_name some_string -list_arg_name some list values
+
 #include <string.h>
 
 int getBoolArg(char *name, int argc, char *argv[]) {
@@ -20,7 +22,7 @@ char* getStrArg(char *name, char *default_value, int argc, char *argv[]) {
 char** getListArg(char *name, char **default_value, int argc, char *argv[]) {
 	int found = 0;
 	int values_number = 0;
-	int allocated = 10;
+	int allocated = 2;
 	char **result = malloc(sizeof(char*) * allocated);
 	int i = 0;
 	for (; i < argc; i++)

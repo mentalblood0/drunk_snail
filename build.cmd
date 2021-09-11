1f94f05c610f1428b5d9a2e1d63255950aef5e72
MentalBlood <neceporenkostepan@gmail.com>
2021-09-11 20:59:19 +0300

    fixed version

--- build.cmd
+++ build.cmd
@@ -1,4 +1,3 @@
 rd /s /q dist drunk_snail\drunk_snail.egg-info
 py -m build -n && ^
-pip install --force-reinstall dist\drunk_snail-1.6-cp39-cp39-win_amd64.whl
-rem py setup.py install
+pip install --force-reinstall dist\drunk_snail-2.0-cp39-cp39-win_amd64.whl
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
 	setup(
 		name='drunk_snail',
-		version='1.6',
+		version='2.0',
 		description='Simple template engine. Faster than you think',
 		long_description=long_description,
 		long_description_content_type='text/markdown',

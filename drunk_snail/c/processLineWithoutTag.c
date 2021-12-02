80f56cb9a466a12c194147a6671f30fa8fcb5c3c
necheporenko_s_iu <neceporenkostepan@gmail.com>
2021-12-02 15:11:04 +0300

    fix for passing test_endpoint_template

--- drunk_snail/c/processLineWithoutTag.c
+++ drunk_snail/c/processLineWithoutTag.c
@@ -6,7 +6,8 @@ compile__cpy_print_left_part();
 addTabs(&result_end, inner_tabs_number);
 if (prefix_start)
 	compile__memcpy(prefix_start, prefix_end);
-compile__memcpy(line_start, line_end);
+if (line_start <= line_end)
+	compile__memcpy(line_start, line_end);
 if (postfix_start)
 	compile__memcpy(postfix_start, postfix_end);
 if ((*c && *(c-1)) || depth) { /* processing not last line */

6a5d4d31f04a233578e63e841a00c3b5e44c5729
mentalblood <neceporenkostepan@gmail.com>
2022-01-23 15:30:19 +0300

    previous commit bugfixes

--- drunk_snail/c/addKeyword.c
+++ drunk_snail/c/addKeyword.c
@@ -8,14 +8,12 @@ static PyObject *addKeyword (
 		*keyword, 
 		*symbol;
 	
-	Template *template = dictionaryLookup(_templates, template_name);
-	addKeywordByKeywords(template->keywords, keyword, symbol);
-	
 	if (!PyArg_ParseTuple(args, "sss", &template_name, &keyword, &symbol)) {
 		return PyLong_FromLong(1);
 	}
 
-	addKeywordByTemplateName(template_name, keyword, symbol[0]);
+	Template *template = dictionaryLookup(_templates, template_name);
+	addKeywordByKeywords(template->keywords, keyword, symbol[0]);
 
 	return PyLong_FromLong(0);
 

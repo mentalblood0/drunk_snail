e75d45398e73ef42f1135b37fcfea79d79301aeb
necheporenko_s_iu <neceporenkostepan@gmail.com>
2022-02-14 16:52:50 +0300

    removed trash

--- tests/test_addTemplate.py
+++
@@ -1,7 +0,0 @@
-from drunk_snail import Template
-from drunk_snail.sources import FileSource
-
-
-
-def test_basic():
-	assert Template('CanonicalizationMethod', FileSource('templates/CanonicalizationMethod.xml'))

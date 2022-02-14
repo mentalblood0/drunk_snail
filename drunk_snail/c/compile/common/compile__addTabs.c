1104ca54d3c332c08bbfb8fdb5bb09314f08af33
necheporenko_s_iu <neceporenkostepan@gmail.com>
2022-02-14 17:04:20 +0300

    optimized addTabs

--- drunk_snail/c/compile/common/compile__addTabs.c
+++ drunk_snail/c/compile/common/compile__addTabs.c
@@ -1,6 +1,14 @@
 int compile__addTabs__i;
 #define compile__addTabs(s_end, n) {\
+	if ((result_end - result + n) >= buffer_size) {\
+		if (!depth) {\
+			free(result);\
+		}\
+		compilation_result->code = 2;\
+		return;\
+	}\
 	for (compile__addTabs__i = 0; compile__addTabs__i < n; compile__addTabs__i++) {\
-		compile__cpy_one('\t');\
+		*result_end = '\t';\
+		result_end++;\
 	}\
 }

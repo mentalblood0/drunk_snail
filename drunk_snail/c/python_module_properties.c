3f2d5baa50f8847dd7524e032430000470f781cc
Stepan Necheporenko <neceporenkostepan@gmail.com>
2022-03-18 15:24:45 +0300

    use pointer to iter through chart_to_escape

--- drunk_snail/c/python_module_properties.c
+++ drunk_snail/c/python_module_properties.c
@@ -48,12 +48,9 @@ PyMODINIT_FUNC PyInit_drunk_snail_c(void) {
 	for (i = 0; i < 128; i++)
 		chars_to_escape_check[i] = 0;
 
-	// char **c = NULL;
-	// for (c = chars_to_escape; **c; c++) {
-	// 	chars_to_escape_check[(int)(**c)] = *(*c + 1);
-	// }
-	for (i = 0; i < 4; i++) {
-		chars_to_escape_check[(int)(chars_to_escape[i][0])] = chars_to_escape[i][1];
+	char *c = NULL;
+	for (c = chars_to_escape; *c; c += 2) {
+		chars_to_escape_check[(int)(*c)] = *(c + 1);
 	}
 
 	return PyModule_Create(&drunk_snail_c_module);

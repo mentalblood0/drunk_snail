5e459d1aa37b2915b4777de56ceea30cc67552c4
Нечепоренко Степан <neceporenkostepan@gmail.com>
2022-06-02 14:54:25 +0300

    add `table_by_fstring` benchmark

--- benchmarks/benchmark_render.py
+++ benchmarks/benchmark_render.py
@@ -5,6 +5,61 @@ from drunk_snail import Template
 from drunk_snail.sources import FileSource
 
 
+
+def generateFString(width, height):
+	return ''.join([
+		'<table>\n'
+		''.join([
+			''.join([
+				'	<tr>\n',
+				''.join([
+					f'		<td>Table["Row"][{j}][{i}]</td>\n'
+					for i in range(width)
+				]),
+				'	</tr>\n'
+			])
+			for j in range(height)
+		]),
+		'</table>'
+	])
+
+
+def getDimension(o: dict | list):
+	return len(o['Row']), len(o['Row'][0]['cell'])
+	return ''.join([
+		f'{k}{getDimension(v)}'
+		for k, v in o.items()
+	] if type(o) == dict
+	else [
+		f'{getDimension(e)}'
+		for e in o
+	] if type(o) == list
+	else ['_'])
+
+
+class table_by_fstring(Benchmark):
+
+	def prepare(self, width, height):
+
+		if not hasattr(self, 'args'):
+			self.fstring = generateFString(width, height)
+			self.args = {
+				"Row": [
+					{
+						"cell": [
+							f"{x}.{y}"
+							for x in range(width)
+						]
+					}
+					for y in range(height)
+				]
+			}
+
+	def run(self, **kwargs):
+		getDimension(self.args)
+		self.fstring.format(Table=self.args)
+
+
 class table(Benchmark):
 
 	def prepare(self, width, height):

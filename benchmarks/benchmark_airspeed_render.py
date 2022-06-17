2e7f9775ccf3c310ed44bb23b1f96dcdabb08eb4
Нечепоренко Степан <neceporenkostepan@gmail.com>
2022-06-17 15:52:38 +0300

    add `airspeed` to engines comparison

---
+++ benchmarks/benchmark_airspeed_render.py
@@ -0,0 +1,35 @@
+import airspeed
+from sharpener import Benchmark
+
+
+
+class table(Benchmark):
+
+	def prepare(self, width, height):
+
+		if not hasattr(self, 'table'):
+
+			self.table = airspeed.Template(
+				'<table>\n'
+				'#foreach ($row in $table)'
+				'	<tr>\n'
+				'#foreach ($value in $row)'
+				'		<td>$value</td>\n'
+				'#end'
+				'	</tr>\n'
+				'#end'
+				'</table>'
+			)
+
+			self.args = {
+				'table': [
+					[
+						f"{x}.{y}"
+						for x in range(width)
+					]
+					for y in range(height)
+				]
+			}
+	
+	def run(self, **kwargs):
+		self.table.merge(self.args)

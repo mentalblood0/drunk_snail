1609d6eddaadbd5edd0367843579bacee7d686eb
Нечепоренко Степан <neceporenkostepan@gmail.com>
2022-08-15 12:43:01 +0300

    move to `sharpener_lite`, reduce boilerplate (use benchmark_test.json)

--- benchmarks/benchmark_render.py
+++ benchmarks/benchmark_render.py
@@ -1,181 +1,165 @@
 import json
 import orjson
-from sharpener_lite import Benchmark
+import functools
+from sharpener_lite import Benchmark, units
 
 from drunk_snail import Template
 from drunk_snail_python import render_hardcoded
 
 
 
-class table(Benchmark):
+class OutputIsRun:
 
-	def prepare(self, width, height):
-
-		if not hasattr(self, 'args'):
-
-			self.row = Template('Row')
-			with open('templates/Row.xml') as f:
-				self.row.register(f.read())
-
-			self.table = Template('Table')
-			with open('templates/Table.xml') as f:
-				self.table.register(f.read())
-
-			self.args = {
-				"Row": [
-					{
-						"cell": [
-							str(x)
-							for x in range(width)
-						]
-					}
-					for y in range(height)
-				]
-			}
-
-	def run(self, **kwargs):
-		self.table(self.args)
+	@property
+	def output(self):
+		return self.run()
 
 
-class table_hardcoded(Benchmark):
+class WithOutputMetrics(OutputIsRun):
 
-	def prepare(self, width, height):
+	@property
+	def metric_output_size(self):
+		return len(self.output) * units.byte
 
-		if not hasattr(self, 'args'):
-
-			self.args = {
-				"Row": [
-					{
-						"cell": [
-							str(x)
-							for x in range(width)
-						]
-					}
-					for y in range(height)
-				]
-			}
-
-	def run(self, **kwargs):
-		render_hardcoded(self.args)
+	@property
+	def metric_output_speed(self):
+		return (self.metric_output_size / self.metric_mean_time).to(units.megabyte / units.second)
 
 
-class table_multiparam(Benchmark):
+class WithTableArgs:
 
-	def prepare(self, width, height):
+	@functools.cached_property
+	def args(self):
+		return {
+			"Row": [
+				{
+					"cell": [
+						str(x)
+						for x in range(self.config.kwargs['width'])
+					]
+				}
+				for y in range(self.config.kwargs['height'])
+			]
+		}
 
-		if not hasattr(self, 'args'):
 
-			with open('templates/cells.xml') as f:
-				Template('cells').register(f.read())
+class table(Benchmark, WithOutputMetrics, WithTableArgs):
 
-			with open('templates/RowMultiparam.xml') as f:
-				Template('RowMultiparam').register(f.read())
+	@functools.cached_property
+	def row(self):
+		result = Template('Row')
+		with open('templates/Row.xml') as f:
+			result.register(f.read())
+		return result
 
-			self.table = Template('TableMultiparam')
-			with open('templates/TableMultiparam.xml') as f:
-				self.table_text = f.read()
-			self.table.register(self.table_text)
+	@functools.cached_property
+	def table(self):
+		self.row
+		result = Template('Table')
+		with open('templates/Table.xml') as f:
+			result.register(f.read())
+		return result
 
-			self.args = {
-				"RowMultiparam": [
-					{
-						"cells": [
-							{
-								'a': str(4*x),
-								'b': str(4*x + 1),
-								'c': str(4*x + 2),
-								'd': str(4*x + 3)
-							}
-							for x in range(width)
-						]
-					}
-					for y in range(height)
-				]
-			}
+	def prepare(self, **kwargs):
+		self.row
+		self.table
+		self.args
 
 	def run(self, **kwargs):
-		self.table(self.args)
+		return self.table(self.args)
 
 
-class args_to_str(Benchmark):
+class table_hardcoded(Benchmark, WithOutputMetrics, WithTableArgs):
 
-	def prepare(self, width, height):
+	def prepare(self, **kwargs):
+		self.args
 
-		if not hasattr(self, 'args'):
-			self.args = {
-				"Row": [
-					{
-						"cell": [
-							str(x)
-							for x in range(width)
-						]
-					}
-					for y in range(height)
-				]
-			}
+	@functools.cached_property
+	def args(self):
+		return {
+			"Row": [
+				{
+					"cell": [
+						str(x)
+						for x in range(self.config.kwargs['width'])
+					]
+				}
+				for y in range(self.config.kwargs['height'])
+			]
+		}
+
+	def run(self, **kwargs):
+		return render_hardcoded(self.args)
+
+
+class table_multiparam(Benchmark, WithOutputMetrics):
+
+	def prepare(self, **kwargs):
+		self.cells
+		self.row_multiparam
+		self.table_multiparam
+		self.args
+
+	def registerFromFile(self, name: str):
+		result = Template(name)
+		with open(f'templates/{name}.xml') as f:
+			result.register(f.read())
+		return result
+
+	@functools.cached_property
+	def cells(self):
+		return self.registerFromFile('cells')
+
+	@functools.cached_property
+	def row_multiparam(self):
+		return self.registerFromFile('RowMultiparam')
+
+	@functools.cached_property
+	def table_multiparam(self):
+		return self.registerFromFile('TableMultiparam')
+
+	@functools.cached_property
+	def args(self):
+		return {
+			"RowMultiparam": [
+				{
+					"cells": [
+						{
+							'a': str(4*x),
+							'b': str(4*x + 1),
+							'c': str(4*x + 2),
+							'd': str(4*x + 3)
+						}
+						for x in range(self.config.kwargs['width'])
+					]
+				}
+				for y in range(self.config.kwargs['height'])
+			]
+		}
+
+	def run(self, **kwargs):
+		return self.table_multiparam(self.args)
+
+
+class args_to_str(Benchmark, WithOutputMetrics, WithTableArgs):
 	
 	def run(self, **kwargs):
-		str(self.args)
+		return str(self.args)
 
 
-class args_to_json(Benchmark):
-
-	def prepare(self, width, height):
-
-		if not hasattr(self, 'args'):
-			self.args = {
-				"Row": [
-					{
-						"cell": [
-							str(x)
-							for x in range(width)
-						]
-					}
-					for y in range(height)
-				]
-			}
+class args_to_json(Benchmark, WithOutputMetrics, WithTableArgs):
 	
 	def run(self, **kwargs):
-		json.dumps(self.args)
+		return json.dumps(self.args)
 
 
-class args_to_json_using_orjson(Benchmark):
-
-	def prepare(self, width, height):
-
-		if not hasattr(self, 'args'):
-			self.args = {
-				"Row": [
-					{
-						"cell": [
-							str(x)
-							for x in range(width)
-						]
-					}
-					for y in range(height)
-				]
-			}
+class args_to_json_using_orjson(Benchmark, WithOutputMetrics, WithTableArgs):
 	
 	def run(self, **kwargs):
-		orjson.dumps(self.args)
+		return orjson.dumps(self.args)
 
 
-class args_to_json_with_indent(Benchmark):
-
-	def prepare(self, width, height, **kwargs):
-
-		if not hasattr(self, 'args'):
-			self.args = {
-				"Row": [
-					{
-						"cell": [
-							str(x)
-							for x in range(width)
-						]
-					}
-					for y in range(height)
-				]
-			}
+class args_to_json_with_indent(Benchmark, WithOutputMetrics, WithTableArgs):
 	
 	def run(self, indent, **kwargs):
-		json.dumps(self.args, indent=indent)
+		return json.dumps(self.args, indent=indent)

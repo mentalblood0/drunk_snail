bc5525655472416e64fde7320c9b7691f4bc7935
Нечепоренко Степан <neceporenkostepan@gmail.com>
2022-10-11 15:43:13 +0300

    fixes for multiparam lines buffer realloations

--- drunk_snail/drunk_snail_python/modules/drunk_snail_c/include/print_macros.h
+++ drunk_snail/drunk_snail_python/modules/drunk_snail_c/include/print_macros.h
@@ -26,7 +26,7 @@
 };
 
 #define render__param(target, OTHER_LEFT, OTHER_LEFT_length, ARG, ARG_length, OTHER_RIGHT, OTHER_RIGHT_length) {\
-	required_buffer_size = ((target) - render_result->result) + (OTHER_LEFT_length+ARG_length+OTHER_RIGHT_length+1) + other_left_length + other_right_length;\
+	required_buffer_size = ((target) - render_result->result) + (OTHER_LEFT_length+ARG_length+OTHER_RIGHT_length) + other_left_length + other_right_length;\
 	if (required_buffer_size >= *buffer_size) {\
 		(*buffer_size) = 2 * required_buffer_size;\
 		drunk_realloc_with_shifted(render_result->result, sizeof(char) * (*buffer_size), render_result->result_temp, target, alloc_error);\
@@ -49,6 +49,14 @@
 };
 
 #define render__param_multi_first(target, OTHER_LEFT, OTHER_LEFT_length, ARG, ARG_length, OTHER_RIGHT, OTHER_RIGHT_length) {\
+	required_buffer_size = ((target) - render_result->result) + (OTHER_LEFT_length+ARG_length+OTHER_RIGHT_length) + other_left_length + other_right_length;\
+	if (required_buffer_size >= *buffer_size) {\
+		(*buffer_size) = 2 * required_buffer_size;\
+		drunk_realloc_with_shifted(render_result->result, sizeof(char) * (*buffer_size), render_result->result_temp, target, alloc_error);\
+		if (alloc_error) {\
+			exit_render_();\
+		}\
+	}\
 	for (i = 0; i < depth; i++) {\
 		drunk_memcpy((target), other->start[i]->left.start, other->start[i]->left.length);\
 	}\
@@ -58,11 +66,27 @@
 };
 
 #define render__param_multi_between(target, ARG, ARG_length, OTHER_RIGHT, OTHER_RIGHT_length) {\
+	required_buffer_size = ((target) - render_result->result) + (ARG_length+OTHER_RIGHT_length+1) + other_left_length + other_right_length;\
+	if (required_buffer_size >= *buffer_size) {\
+		(*buffer_size) = 2 * required_buffer_size;\
+		drunk_realloc_with_shifted(render_result->result, sizeof(char) * (*buffer_size), render_result->result_temp, target, alloc_error);\
+		if (alloc_error) {\
+			exit_render_();\
+		}\
+	}\
 	drunk_memcpy((target), ARG, ARG_length);\
 	drunk_memcpy((target), OTHER_RIGHT, OTHER_RIGHT_length);\
 };
 
 #define render__param_multi_last(target, ARG, ARG_length, OTHER_RIGHT, OTHER_RIGHT_length) {\
+	required_buffer_size = ((target) - render_result->result) + (ARG_length+OTHER_RIGHT_length) + other_left_length + other_right_length;\
+	if (required_buffer_size >= *buffer_size) {\
+		(*buffer_size) = 2 * required_buffer_size;\
+		drunk_realloc_with_shifted(render_result->result, sizeof(char) * (*buffer_size), render_result->result_temp, target, alloc_error);\
+		if (alloc_error) {\
+			exit_render_();\
+		}\
+	}\
 	drunk_memcpy((target), ARG, ARG_length);\
 	drunk_memcpy((target), OTHER_RIGHT, OTHER_RIGHT_length);\
 	if (other_right_length) {\

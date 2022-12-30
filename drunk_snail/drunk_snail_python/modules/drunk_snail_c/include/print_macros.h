e509983153559bba02f802189d50413f5032d0ab
mentalblood <neceporenkostepan@gmail.com>
2022-12-30 22:04:17 +0300

    improve safety of reallocations in prints

--- drunk_snail/drunk_snail_python/modules/drunk_snail_c/include/print_macros.h
+++ drunk_snail/drunk_snail_python/modules/drunk_snail_c/include/print_macros.h
@@ -7,7 +7,7 @@
 #define render__empty(target, LINE, LINE_length) {\
 	required_buffer_size = ((target) - render_result->result) + (LINE_length+1) + other_left_length + other_right_length;\
 	if (required_buffer_size + 8 >= *buffer_size) {\
-		(*buffer_size) = 2 * required_buffer_size;\
+		(*buffer_size) = 2 * required_buffer_size + 8;\
 		drunk_realloc_with_shifted(render_result->result, sizeof(char) * (*buffer_size), render_result->result_temp, target, alloc_error);\
 		if (alloc_error) {\
 			exit_render_();\
@@ -28,7 +28,7 @@
 #define render__param(target, OTHER_LEFT, OTHER_LEFT_length, ARG, ARG_length, OTHER_RIGHT, OTHER_RIGHT_length) {\
 	required_buffer_size = ((target) - render_result->result) + (OTHER_LEFT_length+ARG_length+OTHER_RIGHT_length) + other_left_length + other_right_length;\
 	if (required_buffer_size + 8 >= *buffer_size) {\
-		(*buffer_size) = 2 * required_buffer_size;\
+		(*buffer_size) = 2 * required_buffer_size + 8;\
 		drunk_realloc_with_shifted(render_result->result, sizeof(char) * (*buffer_size), render_result->result_temp, target, alloc_error);\
 		if (alloc_error) {\
 			exit_render_();\
@@ -51,7 +51,7 @@
 #define render__param_multi_first(target, OTHER_LEFT, OTHER_LEFT_length, ARG, ARG_length, OTHER_RIGHT, OTHER_RIGHT_length) {\
 	required_buffer_size = ((target) - render_result->result) + (OTHER_LEFT_length+ARG_length+OTHER_RIGHT_length) + other_left_length + other_right_length;\
 	if (required_buffer_size + 8 >= *buffer_size) {\
-		(*buffer_size) = 2 * required_buffer_size;\
+		(*buffer_size) = 2 * required_buffer_size + 8;\
 		drunk_realloc_with_shifted(render_result->result, sizeof(char) * (*buffer_size), render_result->result_temp, target, alloc_error);\
 		if (alloc_error) {\
 			exit_render_();\
@@ -68,7 +68,7 @@
 #define render__param_multi_between(target, ARG, ARG_length, OTHER_RIGHT, OTHER_RIGHT_length) {\
 	required_buffer_size = ((target) - render_result->result) + (ARG_length+OTHER_RIGHT_length+1) + other_left_length + other_right_length;\
 	if (required_buffer_size + 8 >= *buffer_size) {\
-		(*buffer_size) = 2 * required_buffer_size;\
+		(*buffer_size) = 2 * required_buffer_size + 8;\
 		drunk_realloc_with_shifted(render_result->result, sizeof(char) * (*buffer_size), render_result->result_temp, target, alloc_error);\
 		if (alloc_error) {\
 			exit_render_();\
@@ -81,7 +81,7 @@
 #define render__param_multi_last(target, ARG, ARG_length, OTHER_RIGHT, OTHER_RIGHT_length) {\
 	required_buffer_size = ((target) - render_result->result) + (ARG_length+OTHER_RIGHT_length) + other_left_length + other_right_length;\
 	if (required_buffer_size + 8 >= *buffer_size) {\
-		(*buffer_size) = 2 * required_buffer_size;\
+		(*buffer_size) = 2 * required_buffer_size + 8;\
 		drunk_realloc_with_shifted(render_result->result, sizeof(char) * (*buffer_size), render_result->result_temp, target, alloc_error);\
 		if (alloc_error) {\
 			exit_render_();\
